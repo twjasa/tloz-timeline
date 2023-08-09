0f3a3f6ab770d0ec288ce06ad5365202ca7bc97b
twjasa <twjasa@gmail.com>
2023-08-09 00:21:30 -0400

    feat: add corner triangle with css

--- src/components/Era.tsx
+++ src/components/Era.tsx
@@ -4,12 +4,18 @@ import "./Era.css";
 export const Era = ({ title }: { title: string }) => {
   return (
     <div className="eraContainer">
-      <img src="/src/assets/link_impa.png" className="backgroundImgEra" />
-      <div className="mask1" />
-      <div className="mask2" />
-      <div className="mask3" />
-      <div className="mask4" />
-      <h1 className="title">{title}</h1>
+      <div className="triangleContainer" />
+      <div className="emptyTriangle" />
+      <div className="line0" />
+      <div className="line1" />
+      <div className="line2" />
+      <div className="line3" />
+      <div className="innerContainer" style={{ overflow: "hidden" }}>
+        <img src="/src/assets/link_impa.png" className="backgroundImgEra" />
+        {/* <div className="mask3" /> */}
+        {/* <div className="mask4" /> */}
+        <h1 className="title">{title}</h1>
+      </div>
     </div>
   );
 };
